0dfadd3c0c6fb3b10a49f3495ae7247b3c7756ba
Alondra Correa <alondracorrea@Alondras-MacBook-Air.local>
2026-02-07 11:51:42 -0700

    edits 2026

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -16,16 +16,19 @@ const Experience = () => {
           </p>
         </div>
         <div className="experience-item">
-          <h2 className="experience-title">Internship</h2>
+          <h2 className="experience-title">Software Developer</h2>
           <span className="flex type-date">
             <h3 className="experience-company">BreakAway IT</h3>
             <p className="experience-dates">June 2023 - May 2023</p>
           </span>
           <p className="experience-detail">
-            - Designed and implemented a robust ticketing system usign SQL, Microsoft Azure, and Python for managing tech support requests, organizing ticker notes for over 100 client
-            - Crafted a user-friendlt interface with Javascript, HTML, and CSS, with a grid-based layour, scrollable sections, and aesthetic enhancements to improve over design and UX
-            - Optimized SQL queries for faster data retrieval and implemented PowerShell scripts to automate the storage of media files, systematically organizing them by individual ticket
-            - Developed tools and widgets including a timer to track ticketprogress, an email widget for direct communication, and a pie chart displaying ticket statuses and refined the Homepage Layout
+            <p className="experience-detail">
+            • Designed and implemented a robust ticketing system using SQL, Microsoft Azure, and Python for managing tech support requests, organizing ticket notes for over 100 clients<br />
+            • Crafted a user-friendly interface with Javascript, HTML, and CSS, with a grid-based layout, scrollable sections, and aesthetic enhancements to improve overall design and UX<br />
+            • Optimized SQL queries for faster data retrieval and implemented PowerShell scripts to automate the storage of media files, systematically organizing them by individual ticket<br />
+            • Developed tools and widgets including a timer to track ticket progress, an email widget for direct communication, and a pie chart displaying ticket statuses and refined the Homepage Layout
+          </p>
+
           </p>
         </div>
         <div className="experience-item">
@@ -34,7 +37,7 @@ const Experience = () => {
             <h3 className="experience-company">Arizona State University</h3>
             <p className="experience-dates">2019 - 2023</p>
           </span>
-          <p className="experience-detail">B.S in Computer Science</p>
+          <p className="experience-detail">B.S. in Computer Science</p>
         </div> 
         <div className="experience-item">
           <h2 className="experience-title">Volunteer</h2>
@@ -43,7 +46,7 @@ const Experience = () => {
             <p className="experience-dates">August 2023 - December 2023</p>
           </span>
           <p className="experience-detail">
-            Assisted and organized donations incoming donation items. Helped new clients get a fresh start by providing essential items and supporting self advocacy and autonomy at the Donation Center.
+            Assisted and organized donations incoming donation items. Helped new clients get a fresh start by providing essential items and supporting self advocacy and autonomy at the Donation Center
           </p>
         </div>
          <div className="experience-item">
@@ -53,7 +56,7 @@ const Experience = () => {
             <p className="experience-dates">September 2025 - Current</p>
           </span>
           <p className="experience-detail">
-            Volunteer as a House Helper – assist with household tasks, cleaning, crafting, events, etc. Great mission to provide safe housing to homeless woman, in a nurturing environment.
+            House Helper volunteer – assist with household tasks, cleaning, crafting, events, etc. Great mission to provide safe housing to homeless woman, in a nurturing environment
           </p>
         </div>
       </div>
--- src/routes/Camera.js
+++ src/routes/Camera.js
@@ -3,37 +3,47 @@ import "./Camera.css";
 import { useState } from "react";
 import Back from "../components/Back";
 import hikePhoto from "../assets/hike.jpg";
-import hikePhoto2 from "../assets/hike2.jpg";
 import hikePhoto3 from "../assets/hike3.jpg";
 import hikePhoto4 from "../assets/hike4.jpg";
-import hikePhoto5 from "../assets/hike5.jpg";
-import hikePhoto6 from "../assets/hike6.jpg";
 import hikePhoto7 from "../assets/hike7.jpg";
-import hikePhoto8 from "../assets/hike8.jpg";
 import hikePhoto9 from "../assets/hike9.jpg";
-import hikePhoto10 from "../assets/hike10.jpg";
 import hikePhoto11 from "../assets/hike11.jpg";
 import hikePhoto12 from "../assets/hike12.jpg";
 import hikePhoto13 from "../assets/hike13.jpg";
 import hikePhoto14 from "../assets/hike14.jpg";
 import hikePhoto15 from "../assets/hike15.jpg";
+import hikePhoto16 from "../assets/grand_canyon/grand_canyon.png";
+import hikePhoto17 from "../assets/grand_canyon/grandcanyon1.jpg";
+import hikePhoto18 from "../assets/grand_canyon/grandcanyon2.jpg";
+import hikePhoto19 from "../assets/zion/zion_canyon.png";
+import hikePhoto20 from "../assets/zion/zion1.jpg";
+import hikePhoto21 from "../assets/zion/zion2.jpg";
+import hikePhoto22 from "../assets/zion/zion3.jpg";
+import hikePhoto23 from "../assets/zion/zion4.jpg";
+import hikePhoto24 from "../assets/zion/zion5.jpg";
+
+
 const Camera = () => {
   const hikePhotos = [
     hikePhoto,
-    hikePhoto2,
     hikePhoto3,
     hikePhoto4,
-    hikePhoto5,
-    hikePhoto6,
     hikePhoto7,
-    hikePhoto8,
     hikePhoto9,
-    hikePhoto10,
     hikePhoto11,
     hikePhoto12,
     hikePhoto13,
     hikePhoto14,
     hikePhoto15,
+    hikePhoto16,
+    hikePhoto17,
+    hikePhoto18,
+    hikePhoto19,
+    hikePhoto20,
+    hikePhoto21,
+    hikePhoto22,
+    hikePhoto23,
+    hikePhoto24,
   ];
 
   const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
